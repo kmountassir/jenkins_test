a687866a47ff773f9bdc403f9f08a164e6fe188c
MOUNTASSIR Khalid <Khalid.MOUNTASSIR@cegedim-activ.com>
2025-12-01 14:07:03 +0100

    deuxieme_commit

--- src/main/java/jobintech/main.java
+++ src/main/java/jobintech/main.java
@@ -3,7 +3,7 @@ package jobintech;
 public class main {
     public static void main(String[] args) {
         System.out.println("Bonjour tout le monde !");
-        System.out.println("Ceci est mon premier programme Java.");
+        System.out.println("Ceci est mon premier programme Java.!!");
         System.out.println("La somme de 2 + 3 = " + (2 + 3));
     }
     
